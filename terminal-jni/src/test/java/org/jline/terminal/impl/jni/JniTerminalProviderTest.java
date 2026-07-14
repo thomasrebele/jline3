bf4e6b85efaac4563c6a7a595df96cc28ce7a07d
Thomas Rebele <thomas.rebele@gmail.com>
2026-07-15 01:28:57 +0200

    Test cases for issue 2077

--- terminal-jni/src/test/java/org/jline/terminal/impl/jni/JniTerminalProviderTest.java
+++ terminal-jni/src/test/java/org/jline/terminal/impl/jni/JniTerminalProviderTest.java
@@ -8,20 +8,32 @@
  */
 package org.jline.terminal.impl.jni;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.io.SequenceInputStream;
 import java.nio.charset.Charset;
 
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
 import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 import org.jline.terminal.spi.SystemStream;
+import org.jline.utils.NonBlockingReader;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class JniTerminalProviderTest {
 
@@ -51,5 +63,67 @@ public class JniTerminalProviderTest {
                         null,
                         null);
         assertNotNull(terminal);
+    }
+
+    @Timeout(5)
+    @Test
+    void testReadLineOnEmptyInput() throws IOException {
+        String fileContent = "";
+        InputStream fileStream = new ByteArrayInputStream(fileContent.getBytes());
+        Terminal terminal = TerminalBuilder.builder()
+                .streams(fileStream, System.err)
+                .providers("exec")
+                .build();
+        LineReader reader = LineReaderBuilder.builder().terminal(terminal).build();
+        assertThrows(EndOfFileException.class, () -> reader.readLine("> "));
+    }
+
+    @Timeout(5)
+    @Test
+    void testReadLineAfterEOF() throws IOException {
+        System.setProperty(TerminalBuilder.PROP_CLOSE_MODE, "strict");
+        String fileContent = "abc\ndef\n";
+        InputStream fileStream = new ByteArrayInputStream(fileContent.getBytes());
+        InputStream inputStream = new SequenceInputStream(fileStream, new ByteArrayInputStream((new String("\n")).getBytes()));
+        Terminal terminal = TerminalBuilder.builder()
+                .streams(inputStream, System.err)
+                .providers("exec")
+                .build();
+        LineReader reader = LineReaderBuilder.builder().terminal(terminal).build();
+
+        assertEquals("abc", reader.readLine("> "));
+        assertEquals("def", reader.readLine("> "));
+        assertEquals("", reader.readLine("> "));
+        assertThrows(EndOfFileException.class, () -> reader.readLine("> "));
+    }
+
+    @Timeout(5)
+    @Test
+    void testReadLineOnEmptyInputWithoutExec() throws IOException {
+        String fileContent = "";
+        InputStream fileStream = new ByteArrayInputStream(fileContent.getBytes());
+        Terminal terminal = TerminalBuilder.builder()
+                .streams(fileStream, System.err)
+                .build();
+        LineReader reader = LineReaderBuilder.builder().terminal(terminal).build();
+        assertThrows(EndOfFileException.class, () -> reader.readLine("> "));
+    }
+
+    @Timeout(5)
+    @Test
+    void testReadLineAfterEOFWithoutExec() throws IOException {
+        System.setProperty(TerminalBuilder.PROP_CLOSE_MODE, "strict");
+        String fileContent = "abc\ndef\n";
+        InputStream fileStream = new ByteArrayInputStream(fileContent.getBytes());
+        InputStream inputStream = new SequenceInputStream(fileStream, new ByteArrayInputStream((new String("\n")).getBytes()));
+        Terminal terminal = TerminalBuilder.builder()
+                .streams(inputStream, System.err)
+                .build();
+        LineReader reader = LineReaderBuilder.builder().terminal(terminal).build();
+
+        assertEquals("abc", reader.readLine("> "));
+        assertEquals("def", reader.readLine("> "));
+        assertEquals("", reader.readLine("> "));
+        assertThrows(EndOfFileException.class, () -> reader.readLine("> "));
     }
 }
